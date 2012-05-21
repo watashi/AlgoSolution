94e3d2c2795fa14ace311fd57220d4e822b01e8d
Zejun Wu <zejun.wu@gmail.com>
2012-05-22 00:58:56 +0800

    TCAP 2012; #3

--- topcoder/marathon/76.1.TCAP2012/gao.cpp
+++ topcoder/marathon/76.1.TCAP2012/gao.cpp
@@ -25,8 +25,13 @@ llint startll, endll;
 
 void startTimer() {
     gettimeofday(&startti, NULL);
+#ifdef __WATASHI__
+    startti.tv_sec += 3;
+    startti.tv_usec += 868686;
+#else
     startti.tv_sec += 9;
     startti.tv_usec += 543210;
+#endif
     startll = startti.tv_sec * 1000000LL + startti.tv_usec;
 }
 
@@ -43,9 +48,12 @@ T sqr(T x) {
 
 template<typename T>
 void split(const string& s, T* ret) {
-    std::istringstream iss(s);
-    while (iss >> *ret) {
-        ++ret;
+    const char *p = s.c_str();
+    while (*p != '\0') {
+        // puts(p);
+        char *q;
+        *ret++ = strtol(p, &q, 10);
+        p = q;
     }
 }
 
@@ -61,8 +69,23 @@ typedef double real;
 #define fabs_ fabs
 #else
 typedef float real;
-#define sqrt_ sqrtf
+// #define sqrt_ sqrtf
 #define fabs_ fabsf
+inline float sqrt_(float x){
+	union {
+		int intPart;
+		float floatPart;
+	} convertor;
+	union {
+		int intPart;
+		float floatPart;
+	} convertor2;
+	convertor.floatPart = x;
+	convertor2.floatPart = x;
+	convertor.intPart = 0x1FBCF800 + (convertor.intPart >> 1);
+	convertor2.intPart = 0x5f3759df - (convertor2.intPart >> 1);
+	return 0.5f * (convertor.floatPart + (x * convertor2.floatPart));
+}
 #endif
 
 template<typename T>
@@ -71,6 +94,16 @@ struct Point {
 
     void clear() {
         fill(p, p + DIMENSION, 0);
+    }
+
+    void std() {
+        for (int i = 0; i < DIMENSION; ++i) {
+            if (p[i] < 0) {
+                p[i] = 0;
+            } else if (p[i] > MAXZ) {
+                p[i] = MAXZ;
+            }
+        }
     }
 
     T& operator[](int i) {
@@ -86,6 +119,14 @@ struct Point {
     }
 
     string str() const {
+        static char buf[4096];
+        char* s = buf;
+        for (int i = 0; i < DIMENSION; ++i) {
+            s += sprintf(s, "%d ", p[i]);
+        }
+        *(s - 1) = '\0';
+        return buf;
+        /*
         std::ostringstream oss;
         for (int i = 0; i < DIMENSION; ++i) {
             if (i > 0) {
@@ -94,6 +135,7 @@ struct Point {
             oss << p[i];
         }
         return oss.str();
+        */
     }
 
     Point operator-() const {
@@ -143,141 +185,216 @@ struct Point {
     }
 };
 
+int LIMIT = 100;
+// const int LIMIT = 100;
 const int MAXSTEP = MAXN;
 const real RATE = 1.0;
 const real GRAVITY = 1.0;
 
 int n;
+int cz[MAXN];
 Point<real> p[MAXN];
-Point<int> q[MAXN], r[MAXN];
+Point<int> q[MAXN];
 real mind[MAXN], maxd[MAXN];
-int rank[MAXN][MAXN];
+int rank[MAXN][MAXN], rank0[MAXN][MAXN];
 
-void gao(int step) {
-    static Point<real> v;
+void adjust(int n, int k, real rate = 1.0) {
     static Point<real> w[MAXN];
     static real dw[MAXN], dd[MAXN];
-
     for (int i = 0; i < n; ++i) {
-        if ((i & 0x3f) == 0 && timeout()) {
-            return;
-        }
-        bool skip = true;
+        // w[i] = p[i] - p[k];
+        // dd[i] = dw[i] = w[i].abs();
+        dw[i] = 0;
         for (int j = 0; j < DIMENSION; ++j) {
-            if (r[i][j] == -1) {
-                skip = false;
-            }
+            w[i][j] = p[i][j] - p[k][j];
+            dw[i] += w[i][j] * w[i][j];
         }
-        if (skip) {
+        dd[i] = dw[i] = sqrt_(dw[i]);
+    }
+    sort(dd, dd + n);
+
+    real s = 0.0;
+    Point<real> v;
+    v.clear();
+    for (int i = 0; i < n; ++i) {
+        if (i == k) {
             continue;
         }
-        for (int j = 0; j < n; ++j) {
-            dw[j] = 0;
-            for (int k = 0; k < DIMENSION; ++k) {
-                w[j][k] = p[j][k] - p[i][k];
-                dw[j] += sqr(w[j][k]);
-            }
-            dd[j] = dw[j] = sqrt_(dw[j]);
+        real t = 0.0;
+        if (dw[i] < mind[k]) {
+            t += dw[i] - mind[k];
+        } else if (dw[i] > maxd[k]) {
+            t += dw[i] - maxd[k];
         }
-        sort(dd, dd + n);
-
-        real s = 0.0;
-        v.clear();
-        for (int j = 0; j < n; ++j) {
-            if (i == j) {
-                continue;
-            }
-            real t = 0.0;
-            if (dw[j] < mind[i]) {
-                t += dw[j] - mind[i];
-            } else if (dw[j] > maxd[i]) {
-                t += dw[j] - maxd[i];
-            }
-            t += 0.5 * (dw[j] - dd[rank[i][j]]);
-            real u = t;
-            if (step < 20) {
-                u *= t;
-                if (step < 10) {
-                    u *= t;
-                }
-            }
-            u = fabs_(u);
+        t += dw[i] - dd[rank[k][i]];
+        if (t != 0) {
+            real u = cz[i] * sqrt_(fabs_(t));
             s += u;
-            t = u * t / dw[j];
-            for (int k = 0; k < DIMENSION; ++k) {
-                v[k] += w[j][k] * t;
-            }
-            // v = v + w[j] * t;
-            // fprintf(stderr, "[%d][%d], %lf ; t = %lf\n", i, j, dw[i][j], t);
-        }
-        if (s > 0) {
-            v = v * (1.0 / s);
-        }
-        // fprintf(stderr, "%s\n", v[i].str().c_str());
-        // sp += v[i].abs();
-        for (int j = 0; j < DIMENSION; ++j) {
-            if (r[i][j] != -1) {
-                continue;
-            }
-            p[i][j] += v[j];
-            if (p[i][j] > MAXZ) {
-                p[i][j] = MAXZ;
-            } else if (p[i][j] < 0) {
-                p[i][j] = 0;
+            t *= u / dw[i];
+            // v = v + w[i] * t;
+            for (int j = 0; j < DIMENSION; ++j) {
+                v[j] += w[i][j] * t;
             }
         }
     }
-    // upd();
+
+    if (s > 0) {
+        for (int i = 0; i < DIMENSION; ++i) {
+            if (q[k][i] == -1) {
+                p[k][i] += v[i] / s * rate;
+                if (p[k][i] < 0) {
+                    p[k][i] = 0;
+                } else if (p[k][i] > MAXZ) {
+                    p[k][i] = MAXZ;
+                }
+            }
+        }
+    }
+}
+
+template<typename T>
+void shuffle(int n, T a[MAXN], const vector<pair<int, int> >& todo) {
+    static T tmp[MAXN];
+    for (int i = 0; i < n; ++i) {
+        tmp[i] = a[todo[i].second];
+    }
+    copy(tmp, tmp + n, a);
+}
+
+void rerank(int k, int n, const vector<pair<int, int> >& todo) {
+    pair<int, int> v[MAXN];
+    for (int i = 0; i < n; ++i) {
+        v[i] = make_pair(rank0[todo[k].second][todo[i].second], i);
+    }
+    sort(v, v + n);
+    for (int i = 0; i < n; ++i) {
+        if (i == 0 || v[i].first != v[i - 1].first) {
+            rank[k][v[i].second] = i;
+        } else {
+            rank[k][v[i].second] = rank[k][v[i - 1].second];
+        }
+    }
+}
+
+void gao() {
+    vector<pair<int, int> > todo;
+    for (int i = 0; i < n; ++i) {
+        cz[i] = 0;
+        for (int j = 0; j < DIMENSION; ++j) {
+            if (q[i][j] == -1) {
+                ++cz[i];
+            }
+        }
+        todo.push_back(make_pair(cz[i], i));
+    }
+    sort(todo.begin(), todo.end());
+    for (int i = 0; i < n; ++i) {
+        ::cz[i] = 12 - todo[i].first;
+    }
+
+    shuffle(n, mind, todo);
+    shuffle(n, maxd, todo);
+    shuffle(n, q, todo);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < DIMENSION; ++j) {
+            if (q[i][j] == -1) {
+                p[i][j] = MAXZ / 2 + 50 * (2.0 * rand() / RAND_MAX - 1.0);
+            } else {
+                p[i][j] = q[i][j];
+            }
+        }
+    }
+
+    for (int cc = 1; cc <= DIMENSION; ++cc) {
+        int n = lower_bound(todo.begin(), todo.end(), make_pair(cc, -1)) - todo.begin();
+        int m = lower_bound(todo.begin(), todo.end(), make_pair(cc + 1, -1)) - todo.begin();
+        real rate;
+        if (n >= LIMIT) {
+            n = LIMIT;
+            m = (int)todo.size();
+            rate = M_PI;
+        } else {
+            m = std::min(m, LIMIT);
+            rate = M_E;
+        }
+        fprintf(stderr, "%d_: [%d, %d)\n", cc, n, m);
+
+REDO:
+        // partA
+        fprintf(stderr, "%dA: %lf\n", cc, (double)clock() / CLOCKS_PER_SEC);
+        if (timeout()) {
+            goto RET;
+        }
+        for (int i = n; i < m; ++i) {
+            rerank(i, n, todo);
+        }
+        for (int i = 0; i < LIMIT * 2; ++i) {
+            for (int j = n; j < m; ++j) {
+                if (j % LIMIT == 0 && timeout()) {
+                    goto RET;
+                } else {
+                    adjust(n, j, rate);
+                }
+            }
+            rate *= 0.999;
+        }
+
+        if (n == LIMIT) {
+            goto REDO;
+        }
+        // partB
+        fprintf(stderr, "%dB: %lf\n", cc, (double)clock() / CLOCKS_PER_SEC);
+        if (timeout()) {
+            goto RET;
+        }
+        for (int i = 0; i < m; ++i) {
+            rerank(i, m, todo);
+        }
+        rate = 1.0;
+        for (int i = 0; i < LIMIT; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (j % LIMIT == 0 && timeout()) {
+                    goto RET;
+                } else {
+                    adjust(m, j);
+                }
+            }
+            rate *= 0.999;
+        }
+    }
+
+RET:
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < DIMENSION; ++j) {
+            q[todo[i].second][j] = p[i][j] + .5;
+        }
+    }
 }
 
 struct TheUniverseUnravels {
-    void gao() {
-        while (!timeout()) {
-            for (int i = 0; i < n; ++i) {
-                // p[i] = r[i];
-                for (int j = 0; j < DIMENSION; ++j) {
-                    p[i][j] = r[i][j];
-                    if (p[i][j] == -1) {
-                        p[i][j] = rand() % (MAXZ + 1);
-                    }
-                }
-            }
-            int step = 0;
-            while (!timeout()) {
-                ::gao(step);
-                ++step;
-                // fprintf(stderr, "%d: %lf\n", i, sp);
-            }
-            fprintf(stderr, "step = %d\n", step);
-        }
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < DIMENSION; ++j) {
-                q[i][j] = p[i][j] + .5;
-            }
-        }
-    }
-
     vector<string> predictCoordinates(
-            vector<string> coords,
-            vector<string> ranks,
-            vector<int> minDist,
-            vector <int> maxDist) {
+            const vector<string>& coords,
+            const vector<string>& ranks,
+            const vector<int>& minDist,
+            const vector<int>& maxDist) {
         startTimer();
         n = coords.size();
         for (int i = 0; i < n; ++i) {
-            r[i].parse(coords[i]);
-            q[i] = r[i];
-            for (int j = 0; j < DIMENSION; ++j) {
-                if (q[i][j] == -1) {
-                    q[i][j] = 0;
-                }
-            }
-            split(ranks[i], rank[i]);
+            q[i].parse(coords[i]);
+            split(ranks[i], rank0[i]);
             mind[i] = sqrt_(minDist[i]);
             maxd[i] = sqrt_(maxDist[i]);
         }
 
+        if (n > 1200) {
+            LIMIT = 100;
+        } else if (n > 800) {
+            LIMIT = 128;
+        } else if (n > 400) {
+            LIMIT = 150;
+        } else {
+            LIMIT = n / 2;
+        }
         gao();
 
         vector<string> ret;
@@ -300,6 +417,12 @@ vector<string> readLines() {
     scanf("%d ", &n);
     for (int i = 0; i < n; ++i) {
         fgets(buf, sizeof(buf), stdin);
+        for (int j = 0; buf[j] != '\0'; ++j) {
+            if (buf[j] == '\n') {
+                buf[j] = '\0';
+                break;
+            }
+        }
         ret.push_back(buf);
     }
 
