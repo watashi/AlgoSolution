ced0f4a50859f9ee12258da6cc2a20075fc6095c
Zejun Wu <zejun.wu@gmail.com>
2012-10-26 21:45:33 +0800

    Update Primes Module

--- projecteuler/module/Primes.hs
+++ projecteuler/module/Primes.hs
@@ -1,26 +1,78 @@
-{-# OPTIONS -O2 -optc-O -XBangPatterns #-}
-module Primes (primesToN) where
+{-# OPTIONS -O2 -optc-O3 #-}
+module Primes (
+  primesToN,
+  primesFromMToN,
+  primeDivision,
+  isPrime
+  ) where
 
-import Control.Monad
-import Control.Monad.ST
-import Data.Array.IArray
-import Data.Array.MArray
-import Data.Array.ST
-import Data.Array.Unboxed
+import Control.Arrow ((***))
+import Control.Monad (forM_, when)
+import Control.Monad.ST (ST)
+import Data.Array.ST (STUArray, runSTUArray, newArray, newListArray, readArray, writeArray)
+import Data.Array.Unboxed (UArray, (!), assocs)
+import Data.Int (Int64)
 
+--
+isqrt :: (Integral a, Integral b) => a -> b
 isqrt = floor . sqrt . fromIntegral
 
-primeToNA :: Int -> UArray Int Bool
-primeToNA n = runSTUArray sieve where
-	sieve = do
-		a <- newArray (2, n) True :: ST s (STUArray s Int Bool)
-		let sr = isqrt n
-		forM_ [4, 6 .. n] $ \j -> writeArray a j False
-		forM_ [3, 5 .. sr] $ \i -> do
-			si <- readArray a i
-			when si $
-				forM_ [i * i, i * (i + 2) .. n] $ \j -> writeArray a j False
-		return a
+threshold :: Integral a => a
+threshold = 1000000
 
-primesToN :: Int -> [Int]
-primesToN n = [i | (i, e) <- assocs . primeToNA $ n, e]
+primeList :: Integral a => [a]
+primeList = primesToN threshold
+
+-- primesToN
+sieveToN :: Int -> UArray Int Bool
+sieveToN n = runSTUArray sieve
+  where
+	  sieve = do
+		  a <- newArray (2, n) True :: ST s (STUArray s Int Bool)
+		  forM_ [4, 6 .. n] $ \j -> writeArray a j False
+		  forM_ [3, 5 .. isqrt n] $ \i -> do
+			  ai <- readArray a i
+			  when ai $
+				  forM_ [i * i, i * (i + 2) .. n] $ \j -> writeArray a j False
+		  return a
+
+primesToN :: (Integral a, Integral b) => a -> [b]
+primesToN n = map (fromIntegral . fst) $ filter snd $ assocs $ sieveToN n'
+  where
+    n' = fromIntegral n
+
+-- primesFromMToN
+sieveFromMToN :: Int64 -> Int64 -> UArray Int64 Bool
+sieveFromMToN m n = runSTUArray sieve
+  where
+    n' = isqrt n
+    primes = if n' > threshold then primesToN n' else takeWhile (<=n') primeList
+    sieve = do
+      a <- newArray (m, n) True :: ST s (STUArray s Int64 Bool)
+      forM_ primes $ \i -> do
+        let s = max i $ div (m + i - 1) i
+        when (i * s <= n) $
+          forM_ [i * s, i * (s + 1) .. n] $ \j -> writeArray a j False
+      return a
+
+primesFromMToN :: (Integral a, Integral b) => a -> a -> [b]
+primesFromMToN m n = map (fromIntegral . fst) $ filter snd $ assocs $ sieveFromMToN m' n'
+  where
+    m' = fromIntegral m
+    n' = fromIntegral n
+
+-- primeDivision
+primeDivision :: Integral a => a -> [(a, Int)]
+primeDivision n = f n $ primeList ++ [threshold + 1, threshold + 3 ..]
+  where
+    f n q
+      | n == 1        = []
+      | mod n p /= 0  = [(n, 1)]
+      | otherwise     = (p, r): f m t
+      where
+        (p: t) = dropWhile (\i -> i * i <= n && mod n i /= 0) q
+        (m, r) = until (\(i, j) -> mod i p /= 0) ((`div` p) *** succ) (n, 0)
+
+-- isPrime
+isPrime :: Integral a => a -> Bool
+isPrime = null . tail . primeDivision
