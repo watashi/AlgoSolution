7505af6b538326ad6ad2a90ef7710025d2bf52dc
Zejun Wu <zejun.wu@gmail.com>
2012-11-10 21:56:43 +0800

    Update template.cc and dir structure

--- topcoder/template/template.cpp
+++ topcoder/template/template.cpp
@@ -27,31 +27,29 @@ using namespace std;
 typedef long long llint;
 typedef pair<int, int> PII;
 $BEGINCUT$
-#define WATASHI_PC
-
 inline void errf(const char *fmt, ...) {
-	va_list args;
-	va_start(args, fmt);
-	vfprintf(stderr, fmt, args);
-	va_end(args);
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  va_end(args);
 }
 
 template<typename T>
 inline void errf(const char *fmt, const vector<T>& v) {
-	errf("{");
-	FOR(i, v.size()) {
-		errf(fmt, v[i]);
-	}
-	errf("}\n");
+  errf("{");
+  FOR(i, v.size()) {
+    errf(fmt, v[i]);
+  }
+  errf("}\n");
 }
 // TODO
 $ENDCUT$
-#ifndef WATASHI_PC
+#ifndef __WATASHI__
 #define errf(fmt, ...) do { } while (false)
 #endif
 
 struct $CLASSNAME$ {
-	$RC$ $METHODNAME$($METHODPARMS$);
+  $RC$ $METHODNAME$($METHODPARMS$);
 };
 
 $RC$ $CLASSNAME$::$METHODNAME$($METHODPARMS$) {
@@ -64,39 +62,39 @@ $BEGINCUT$
 
 template<typename T>
 ostream& operator<<(ostream& os, vector<T>& v) {
-	os << "{";
-	FOR(i, v.size()) {
-		os << "\"" << v[i] << "\",";
-	}
-	os << "}";
+  os << "{";
+  FOR(i, v.size()) {
+    os << "\"" << v[i] << "\",";
+  }
+  os << "}";
 
-	return os;
+  return os;
 }
 
 template<typename S, typename T>
 void eq(int id, S v1, T v2) {
-	ostringstream s1, s2;
+  ostringstream s1, s2;
 
-	s1 << v1;
-	s2 << v2;
+  s1 << v1;
+  s2 << v2;
 
-	errf("Case #%d: ", id);
-	if (s1.str() == s2.str()) {
-		errf("\033[1;32mPassed\033[0m\n");
-	} else {
-		errf("\033[1;31mFailed\033[0m\n");
-		errf("\tReceived \"%s\"\n", s1.str().c_str());
-		errf("\tExpected \"%s\"\n", s2.str().c_str());
-	}
+  errf("Case #%d: ", id);
+  if (s1.str() == s2.str()) {
+    errf("\033[1;32mPassed\033[0m\n");
+  } else {
+    errf("\033[1;31mFailed\033[0m\n");
+    errf("\tReceived \"%s\"\n", s1.str().c_str());
+    errf("\tExpected \"%s\"\n", s2.str().c_str());
+  }
 }
 
 int main(int argc, char *argv[]) {
-	$MAINBODY$
+  $MAINBODY$
 
-	puts("\033[1;33mPress any key to continue...\033[0m");
-	getchar();
+  puts("\033[1;33mPress any key to continue...\033[0m");
+  getchar();
 
-	return 0;
+  return 0;
 }
 
 /*
