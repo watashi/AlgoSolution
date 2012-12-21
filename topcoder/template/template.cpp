310dba7b416950b7d6a485051c5cd791eab9387c
Zejun Wu <zejun.wu@gmail.com>
2012-12-21 16:24:24 +0800

    Update template.cc to provide better summary info

--- topcoder/template/template.cpp
+++ topcoder/template/template.cpp
@@ -71,6 +71,8 @@ ostream& operator<<(ostream& os, vector<T>& v) {
   return os;
 }
 
+vector<bool> __eq__;
+
 template<typename S, typename T>
 void eq(int id, S v1, T v2) {
   ostringstream s1, s2;
@@ -80,8 +82,10 @@ void eq(int id, S v1, T v2) {
 
   errf("Case #%d: ", id);
   if (s1.str() == s2.str()) {
+    __eq__.push_back(true);
     errf("\033[1;32mPassed\033[0m\n");
   } else {
+    __eq__.push_back(false);
     errf("\033[1;31mFailed\033[0m\n");
     errf("\tReceived \"%s\"\n", s1.str().c_str());
     errf("\tExpected \"%s\"\n", s2.str().c_str());
@@ -94,7 +98,19 @@ int main(int argc, char *argv[]) {
 
   $MAINBODY$
 
-  puts("\033[1;33mPress any key to continue...\033[0m");
+  int __pass__ = count(ALL(__eq__), true);
+  int __fail__ = count(ALL(__eq__), false);
+  if (__fail__ == 0) {
+    errf("\033[1;32mAll %d Passed\033[0m\n", __pass__);
+  } else if (__pass__ == 0) {
+    errf("\033[1;31mAll %d Failed\033[0m\n", __fail__);
+  } else {
+    errf("\033[1;31m%d Passed; %d Failed\033[0m\n", __pass__, __fail__);
+    if (__eq__[0]) {
+      errf("\033[1;31mDid you reset global variables?\033[0m\n");
+    }
+  }
+  errf("\033[1;33mPress any key to continue...\033[0m\n");
   getchar();
 
   return 0;
@@ -110,4 +126,3 @@ int main(int argc, char *argv[]) {
  * vim: ft=cpp.doxygen
  */
 $ENDCUT$
-
