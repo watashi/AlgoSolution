62fa4855fab72d094a9a67ecec2cd92ca4210b2b
Zejun Wu <zejun.wu@gmail.com>
2013-05-06 02:20:30 +0800

    First submission

--- topcoder/marathon/80/gao.cpp
+++ topcoder/marathon/80/gao.cpp
@@ -1,3 +1,4 @@
+#include <map>
 #include <cmath>
 #include <ctime>
 #include <limits>
@@ -32,32 +33,25 @@ const llint TIME_LIMIT = 9500000;
 static timeval tv;
 static llint startll, endll;
 
-void startTimer() {
+inline void startTimer() {
   gettimeofday(&tv, NULL);
   startll = tv.tv_sec * MSEC_PER_SEC + tv.tv_usec + TIME_LIMIT;
 }
 
-bool timeout() {
+inline bool timeout() {
   gettimeofday(&tv, NULL);
   endll = tv.tv_sec * MSEC_PER_SEC + tv.tv_usec;
   return endll > startll;
 }
 
 const int MAXN = 105;
-
+const int R = 0, D = 1, U = 2, L = 3;
 const int dx[4] = {0, 1, -1, 0};
 const int dy[4] = {1, 0, 0, -1};
 
-enum {
-  R = 0,
-  D = 1,
-  U = 2,
-  L = 3
-};
-
 struct Node {
   Node *p[4];
-  int type;
+  int type, hash;
 };
 
 typedef pair<Node*, int> Entry;
@@ -73,6 +67,7 @@ inline void init(const vector<string>& board) {
   sz = board[0].size();
   FOR (i, sz + 2) {
     FOR (j, sz + 2) {
+      cell[i][j].hash = rand();
       if (1 <= i && i <= sz && 1 <= j && j <= sz) {
         cell[i][j].type = board[i - 1][j - 1] == 'R' ? 1 : 2;
       }
@@ -106,25 +101,38 @@ inline void atach(Node* cur) {
   cur->p[R]->p[L] = cur;
 }
 
-inline vector<Node*> remove(Node* cur, int dir) {
-  vector<Node*> ret;
+inline int remove(Node* cur, int dir, vector<Node*>& ret) {
+  int hash = 0;
   while (true) {
     cur = cur->p[dir];
     if (cur->type == 0) {
       break;
     } else {
+      hash ^= cur->hash;
       detach(cur);
       ret.push_back(cur);
       dir ^= cur->type;
     }
   }
-  return ret;
+  return hash;
 }
 
-inline void restore(const vector<Node*>& v) {
+inline int restore(const vector<Node*>& v) {
+  int hash = 0;
   for (vector<Node*>::const_reverse_iterator it = v.rbegin(); it != v.rend(); ++it) {
+    hash ^= (*it)->hash;
     atach(*it);
   }
+  return hash;
+}
+
+inline void compact(vector<Entry>& entry) {
+  FOREACH (i, entry) {
+    if (i->first->p[i->second]->type == 0) {
+      *i = NIL;
+    }
+  }
+  entry.erase(remove(ALL(entry), NIL), entry.end());
 }
 
 inline bool greedy(vector<Entry> entry, int limit, vector<Entry>& ret) {
@@ -136,7 +144,8 @@ inline bool greedy(vector<Entry> entry, int limit, vector<Entry>& ret) {
     Entry best;
     int size = -1;
     FOREACH (i, entry) {
-      vector<Node*> path = remove(i->first, i->second);
+      vector<Node*> path;
+      remove(i->first, i->second, path);
       restore(path);
       if ((int)path.size() > size) {
         size = (int)path.size();
@@ -144,27 +153,87 @@ inline bool greedy(vector<Entry> entry, int limit, vector<Entry>& ret) {
       }
     }
 
-    vector<Node*> path = remove(best.first, best.second);
-    FOREACH (i, entry) {
-      if (i->first->p[i->second]->type == 0) {
-        *i = NIL;
-      }
-    }
-    entry.erase(remove(ALL(entry), NIL), entry.end());
-    ret.push_back(best);
+    vector<Node*> path;
+    bool flag;
 
-    bool flag = greedy(entry, limit, ret);
+    remove(best.first, best.second, path);
+    ret.push_back(best);
+    compact(entry);
+    flag = greedy(entry, limit, ret);
     restore(path);
     return flag;
   }
 }
 
-static vector<Entry> temp, best;
+static vector<Entry> best;
+
+inline void search(int limit) {
+  vector<vector<Entry> > todo;
+  todo.push_back(vector<Entry>());
+  for (int step = 1; !timeout() && step < (int)best.size(); ++step) {
+    map<pair<int, int>, vector<Entry> > top;
+    FOREACH (i, todo) {
+      vector<Node*> done;
+      vector<Entry> temp = *i, entry = ::entry;
+      int hash = 0;
+      FOREACH (j, temp) {
+        hash ^= remove(j->first, j->second, done);
+      }
+      int size = (int)done.size();
+      compact(entry);
+
+      FOREACH (j, entry) {
+        vector<Node*> path;
+        hash ^= remove(j->first, j->second, path);
+        size += (int)path.size();
+        temp.push_back(*j);
+        if (size == sz * sz) {
+#ifdef __WATASHI__
+          clog << "r" << limit << " = " << step << endl;
+#endif
+          best = temp;
+          restore(path);
+          restore(done);
+          return;
+        } else if ((int)top.size() < limit) {
+          top[make_pair(size, hash)] = temp;
+        } else if (make_pair(size, hash) > top.begin()->first) {
+          top.erase(top.begin());
+          top[make_pair(size, hash)] = temp;
+        }
+        temp.pop_back();
+        size -= (int)path.size();
+        hash ^= restore(path);
+      }
+
+      restore(done);
+      if (timeout()) {
+        return;
+      }
+    }
+    todo.clear();
+    FOREACH (j, top) {
+      todo.push_back(j->second);
+    }
+  }
+}
 
 struct FragileMirrors {
-  vector<int> destory(const vector<string>& board) {
+  vector<int> destroy(const vector<string>& board) {
+    startTimer();
     init(board);
     greedy(entry, sz * sz, best);
+#ifdef __WATASHI__
+    clog << "n = " << board.size() << endl;
+    clog << "r1 = " << best.size() << endl;
+#endif
+    for (int i = 2; !timeout(); ++i) {
+      search(i);
+    }
+#ifdef __WATASHI__
+    clog << "time = " << (double)clock() / CLOCKS_PER_SEC << endl;
+    clog << "score = " << (double)board.size() / best.size() << endl;
+#endif
 
     vector<int> ret;
     FOR (i, best.size()) {
@@ -188,12 +257,7 @@ int main() {
     cin >> board[i];
   }
 
-  ret = FragileMirrors().destory(board);
-  clog << "n = " << n << endl;
-  clog << "r = " << ret.size() << endl;
-  clog << "n/r = " << 1.0 * n / ret.size() << endl;
-  clog << "time = " << 1.0 * clock() / CLOCKS_PER_SEC << endl;
-
+  ret = FragileMirrors().destroy(board);
   cout << ret.size() << endl;
   for (int i = 0; i < (int)ret.size(); ++i) {
     cout << ret[i] << endl;
