573ce677682560707c54a8bdb32edb2f047924af
Zejun Wu <watashi@watashi.ws>
2021-12-23 23:51:16 -0800

    Rewrite 1618G w/ BufWriter and w/o BTreeMap

--- codeforces/ac/16/1618/G.rs
+++ codeforces/ac/16/1618/G.rs
@@ -1,9 +1,8 @@
-use std::collections::*;
-use std::convert::*;
+use std::io::*;
 
 fn read_line() -> String {
     let mut line = String::new();
-    std::io::stdin().read_line(&mut line).unwrap();
+    stdin().read_line(&mut line).unwrap();
     line
 }
 
@@ -24,11 +23,11 @@ fn main() {
     for i in read_vec::<i64>() {
         a.push((i, false));
     }
-    a.sort();
+    a.sort_unstable();
 
     let mut xtot = 0;
     let query = {
-        let (mut xcnt, mut xsum, mut cnt, mut sum) = (0usize, 0, vec![0usize], vec![0]);
+        let (mut xcnt, mut xsum, mut cnt, mut sum) = (0, 0, vec![0], vec![0]);
         for i in &a {
             xsum += i.0;
             if i.1 {
@@ -40,42 +39,38 @@ fn main() {
         }
         move |from, to| {
             let c = cnt[to] - cnt[from];
-            sum[to] - sum[to - c]
+            sum[to] - sum[to - c as usize]
         }
     };
 
-    let mut m: BTreeMap<i64, Vec<usize>> = BTreeMap::new();
+    let mut gaps: Vec<(i64, usize)> = vec![];
     for i in 1..a.len() {
-        let k = a[i].0 - a[i - 1].0;
-        match m.get_mut(&k) {
-            Some(v) => v.push(i),
-            None => {
-                m.insert(k, vec![i]);
-            }
-        }
+        gaps.push((a[i].0 - a[i - 1].0, i));
     }
+    gaps.sort_unstable();
 
     let mut prev: Vec<usize> = (0..a.len()).collect();
     let mut next = prev.clone();
     let mut ans: Vec<(i64, i64)> = vec![];
-    ans.push((-1, xtot));
-    for (k, v) in m {
-        for i in v {
-            xtot -= query(prev[i - 1], i);
-            xtot -= query(i, next[i] + 1);
-            xtot += query(prev[i - 1], next[i] + 1);
-            next[prev[i - 1]] = next[i];
-            prev[next[i]] = prev[i - 1];
+    let mut gap: i64 = -1;
+    for (k, i) in gaps {
+        if k != gap {
+            ans.push((gap, xtot));
+            gap = k;
         }
-        ans.push((k, xtot));
+        xtot -= query(prev[i - 1], i);
+        xtot -= query(i, next[i] + 1);
+        xtot += query(prev[i - 1], next[i] + 1);
+        next[prev[i - 1]] = next[i];
+        prev[next[i]] = prev[i - 1];
     }
+    ans.push((gap, xtot));
 
+    let mut stdout = BufWriter::new(stdout());
     for i in read_vec::<i64>() {
-        let k = ans
-            .binary_search(&(i + 1, -1))
-            .map_or_else(identity, identity);
-        println!("{}", ans[k - 1].1);
+        let k = ans.binary_search(&(i + 1, -1)).map_or_else(|x| x, |x| x);
+        writeln!(stdout, "{}", ans[k - 1].1).unwrap();
     }
 }
 
-// 140412263	Dec/23/2021 23:14UTC-8	watashi	1618G - Trader Problem	Rust 2021	Accepted	1762 ms	31800 KB
+// 140414459	Dec/23/2021 23:45UTC-8	watashi	1618G - Trader Problem	Rust 2021	Accepted	171 ms	30700 KB
